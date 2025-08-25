69a95e8bebad9a6f18ecb4b644e6b3e909735c98
Elikem-Ahlijah <62994790+Elikem-Ahlijah@users.noreply.github.com>
2025-08-25 15:40:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def initialize_components():
             model_name="sentence-transformers/all-MiniLM-L6-v2",
             model_kwargs={
                 "device": "cpu",
-                "normalize_embeddings": True
+              #  "normalize_embeddings": True
             },
             # encode_kwargs={"normalize_embeddings": True}
         )
@@ -313,4 +313,5 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
 
